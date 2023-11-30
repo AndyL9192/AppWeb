d8acd6e2ee6ec362535046973b5314ccdb3f00c1
Andres <andylaz92@outlook.com>
2023-11-30 20:52:57 -0300

    iniciando proyecto

---
+++ src/components/button.jsx
@@ -0,0 +1,6 @@
+export const button = ({ texto = "default", variante = "blue" }) => {
+  if (variante === blue) clase = "btn-red";
+  if (variante === green) clase = "btn-red";
+
+  if (variante === red) clase = "btn-red";
+};
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import reactDOM from "react-dom/client";
+
+const element = document.getElementById("root");
+
+const root = reactDOM.createRoot(element);
+
+root.render(<h1>Hello world</h1>);
